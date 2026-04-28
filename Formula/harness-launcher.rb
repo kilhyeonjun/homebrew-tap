7696e82d4678f104256b6c6454b8058f94d666a9
kilhyeonjun <kboxstar@gmail.com>
2026-04-29 08:44:41 +0900

    harness-launcher 0.7.0

--- Formula/harness-launcher.rb
+++ Formula/harness-launcher.rb
@@ -2,8 +2,8 @@ class HarnessLauncher < Formula
   desc "Universal launcher for kilhyeonjun/gameduo harness repos"
   homepage "https://github.com/kilhyeonjun/harness-launcher"
   url "https://github.com/kilhyeonjun/harness-launcher.git",
-      tag: "v0.6.0"
-  version "0.6.0"
+      tag: "v0.7.0"
+  version "0.7.0"
   license "MIT"
 
   def install
