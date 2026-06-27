85ae62c2166b3ffaa7d212021a8f55fb9a630c80
kilhyeonjun <kboxstar@gmail.com>
2026-06-27 16:00:52 +0900

    harness-launcher 0.8.11: bash 3.2 nounset fix for empty managed_names

--- Formula/harness-launcher.rb
+++ Formula/harness-launcher.rb
@@ -2,7 +2,7 @@ class HarnessLauncher < Formula
   desc "Universal launcher for kilhyeonjun/gameduo harness repos"
   homepage "https://github.com/kilhyeonjun/harness-launcher"
   url "https://github.com/kilhyeonjun/harness-launcher.git",
-      tag: "v0.8.10"
+      tag: "v0.8.11"
   license "MIT"
 
   def install
